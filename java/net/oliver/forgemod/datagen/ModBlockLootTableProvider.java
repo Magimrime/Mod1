6d6a321e6a375b1a59892f7a0c89c21855d32bd4
Magimrime <magimrimefilms@gmail.com>
2025-08-28 17:45:13 -0700

    Add files via upload
    got it working, but for some reason the github disconnected

---
+++ java/net/oliver/forgemod/datagen/ModBlockLootTableProvider.java
@@ -0,0 +1,144 @@
+package net.oliver.forgemod.datagen;
+
+import net.minecraft.advancements.critereon.StatePropertiesPredicate;
+import net.minecraft.core.Direction;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.data.loot.BlockLootSubProvider;
+import net.minecraft.world.flag.FeatureFlag;
+import net.minecraft.world.flag.FeatureFlagSet;
+import net.minecraft.world.flag.FeatureFlags;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.enchantment.Enchantment;
+import net.minecraft.world.item.enchantment.Enchantments;
+import net.minecraft.world.level.ItemLike;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.MultifaceBlock;
+import net.minecraft.world.level.block.SweetBerryBushBlock;
+import net.minecraft.world.level.storage.loot.LootPool;
+import net.minecraft.world.level.storage.loot.LootTable;
+import net.minecraft.world.level.storage.loot.entries.LootItem;
+import net.minecraft.world.level.storage.loot.entries.LootPoolEntryContainer;
+import net.minecraft.world.level.storage.loot.entries.LootPoolSingletonContainer;
+import net.minecraft.world.level.storage.loot.functions.ApplyBonusCount;
+import net.minecraft.world.level.storage.loot.functions.SetItemCountFunction;
+import net.minecraft.world.level.storage.loot.predicates.BonusLevelTableCondition;
+import net.minecraft.world.level.storage.loot.predicates.LootItemBlockStatePropertyCondition;
+import net.minecraft.world.level.storage.loot.predicates.LootItemCondition;
+import net.minecraft.world.level.storage.loot.predicates.LootItemRandomChanceCondition;
+import net.minecraft.world.level.storage.loot.providers.number.ConstantValue;
+import net.minecraft.world.level.storage.loot.providers.number.UniformGenerator;
+import net.minecraft.world.item.Items;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.registries.RegistryObject;
+import net.oliver.forgemod.block.ModBlocks;
+import net.oliver.forgemod.item.ModItems;
+
+import java.util.Set;
+
+public class ModBlockLootTableProvider extends BlockLootSubProvider {
+    protected ModBlockLootTableProvider(HolderLookup.Provider pRegistries) {
+        super(Set.of(), FeatureFlags.REGISTRY.allFlags(), pRegistries);
+    }
+
+    @Override
+    protected void generate() {
+        this.dropSelf(ModBlocks.WALNUT_LOG.get());
+        this.dropSelf(ModBlocks.WALNUT_WOOD.get());
+        this.dropSelf(ModBlocks.STRIPPED_WALNUT_LOG.get());
+        this.dropSelf(ModBlocks.STRIPPED_WALNUT_WOOD.get());
+        this.dropSelf(ModBlocks.WALNUT_PLANKS.get());
+        this.dropSelf(ModBlocks.WALNUT_SAPLING.get());
+
+        // Wooden blocks that drop themselves
+        this.dropSelf(ModBlocks.WALNUT_STAIRS.get());
+        this.dropSelf(ModBlocks.WALNUT_SLAB.get());
+        this.dropSelf(ModBlocks.WALNUT_PRESSURE_PLATE.get());
+        this.dropSelf(ModBlocks.WALNUT_BUTTON.get());
+        this.dropSelf(ModBlocks.WALNUT_FENCE.get());
+        this.dropSelf(ModBlocks.WALNUT_FENCE_GATE.get());
+        this.dropSelf(ModBlocks.WALNUT_TRAPDOOR.get());
+
+        this.add(ModBlocks.WALNUT_DOOR.get(),
+                block -> createDoorTable(ModBlocks.WALNUT_DOOR.get()));
+
+
+        this.add(ModBlocks.WALNUT_LEAVES.get(), block ->
+                createWalnutLeavesDrops(block, ModBlocks.WALNUT_SAPLING.get(), NORMAL_LEAVES_SAPLING_CHANCES));
+
+    }
+
+    protected LootTable.Builder createMultipleOreDrops(Block pBlock, Item item, float minDrops, float maxDrops) {
+        HolderLookup.RegistryLookup<Enchantment> registrylookup = this.registries.lookupOrThrow(Registries.ENCHANTMENT);
+        return this.createSilkTouchDispatchTable(
+                pBlock, this.applyExplosionDecay(
+                        pBlock, LootItem.lootTableItem(item)
+                                .apply(SetItemCountFunction.setCount(UniformGenerator.between(minDrops, maxDrops)))
+                                .apply(ApplyBonusCount.addOreBonusCount(registrylookup.getOrThrow(Enchantments.FORTUNE)))
+                )
+        );
+
+    }
+
+    protected LootTable.Builder createMultipeOreDrops(Block pBlock, Item item, float minDrops, float maxDrops) {
+        HolderLookup.RegistryLookup<Enchantment> registrylookup = this.registries.lookupOrThrow(Registries.ENCHANTMENT);
+        return this.createSilkTouchDispatchTable(
+                pBlock, this.applyExplosionDecay(
+                        pBlock, LootItem.lootTableItem(item)
+                                .apply(SetItemCountFunction.setCount(UniformGenerator.between(minDrops, maxDrops)))
+                                .apply(ApplyBonusCount.addOreBonusCount(registrylookup.getOrThrow(Enchantments.FORTUNE)))
+                )
+        );
+    }
+
+    protected static LootTable.Builder createShearsOnlyDrop(ItemLike pItem) {
+        return LootTable.lootTable()
+                .withPool(LootPool.lootPool().setRolls(ConstantValue.exactly(1.0F)).when(HAS_SHEARS).add(LootItem.lootTableItem(pItem)));
+    }
+
+    protected LootTable.Builder createMultifaceBlockDrops(Block pBlock, LootItemCondition.Builder pBuilder) {
+        return LootTable.lootTable()
+                .withPool(
+                        LootPool.lootPool()
+                                .add(
+                                        (LootPoolEntryContainer.Builder<?>)this.applyExplosionDecay(
+                                                pBlock,
+                                                LootItem.lootTableItem(pBlock)
+                                                        .when(pBuilder)
+                                                        .apply(
+                                                                Direction.values(),
+                                                                p_251536_ -> SetItemCountFunction.setCount(ConstantValue.exactly(1.0F), true)
+                                                                        .when(
+                                                                                LootItemBlockStatePropertyCondition.hasBlockStateProperties(pBlock)
+                                                                                        .setProperties(StatePropertiesPredicate.Builder.properties().hasProperty(MultifaceBlock.getFaceProperty(p_251536_), true))
+                                                                        )
+                                                        )
+                                                        .apply(SetItemCountFunction.setCount(ConstantValue.exactly(-1.0F), true))
+                                        )
+                                )
+                );
+    }
+
+    @Override
+    protected Iterable<Block> getKnownBlocks() {
+        return ModBlocks.BLOCKS.getEntries().stream().map(RegistryObject::get)::iterator;
+    }
+
+    protected LootTable.Builder createWalnutLeavesDrops(Block pWalnutLeavesBlock, Block pSaplingBlock, float... pChances) {
+        HolderLookup.RegistryLookup<Enchantment> registrylookup = this.registries.lookupOrThrow(Registries.ENCHANTMENT);
+        return this.createLeavesDrops(pWalnutLeavesBlock, pSaplingBlock, pChances)
+                .withPool(
+                        LootPool.lootPool()
+                                .setRolls(ConstantValue.exactly(1.0F))
+                                .when(this.doesNotHaveSilkTouch())
+                                .add(
+                                        ((LootPoolSingletonContainer.Builder)this.applyExplosionCondition(pWalnutLeavesBlock, LootItem.lootTableItem(ModItems.WALNUT.get())))
+                                                .when(
+                                                        BonusLevelTableCondition.bonusLevelFlatChance(
+                                                                registrylookup.getOrThrow(Enchantments.FORTUNE), 0.0525F, 0.075F, 0.09F, 0.125F
+                                                        )
+                                                )
+                                )
+                );
+    }
+}
---
+++ java/net/oliver/forgemod/worldgen/ModBiomeModifiers.java
@@ -0,0 +1,39 @@
+package net.oliver.forgemod.worldgen;
+
+import net.minecraft.core.HolderSet;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.data.worldgen.BootstrapContext;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.BiomeTags;
+import net.minecraft.world.level.biome.Biomes;
+import net.minecraft.world.level.biome.MobSpawnSettings;
+import net.minecraft.world.level.levelgen.GenerationStep;
+import net.minecraftforge.common.world.BiomeModifier;
+import net.minecraftforge.common.world.ForgeBiomeModifiers;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.oliver.forgemod.ForgeMod;
+import net.oliver.forgemod.entity.ModEntities;
+import net.oliver.forgemod.worldgen.biome.ModBiomes;
+
+import java.util.List;
+
+public class ModBiomeModifiers {
+    public static final ResourceKey<BiomeModifier> ADD_WALNUT_TREE = registerKey("add_tree_walnut");
+
+    public static final ResourceKey<BiomeModifier> SPAWN_DIRT_SNAIL = registerKey("spawn_dirt_snail");
+
+    public static void bootstrap(BootstrapContext<BiomeModifier> context) {
+        var placedFeature = context.lookup(Registries.PLACED_FEATURE);
+        var biomes = context.lookup(Registries.BIOME);
+
+        context.register(ADD_WALNUT_TREE, new ForgeBiomeModifiers.AddFeaturesBiomeModifier(
+                HolderSet.direct(biomes.getOrThrow(Biomes.THE_VOID)),
+                HolderSet.direct(placedFeature.getOrThrow(ModPlacedFeatures.WALNUT_PLACED_KEY)),
+                GenerationStep.Decoration.VEGETAL_DECORATION));
+    }
+
+    private static ResourceKey<BiomeModifier> registerKey(String name) {
+        return ResourceKey.create(ForgeRegistries.Keys.BIOME_MODIFIERS, ResourceLocation.fromNamespaceAndPath(ForgeMod.MOD_ID, name));
+    }
+}
